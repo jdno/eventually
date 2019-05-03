e22344459a5cae8e9377796770d612b2fd14b0d7
Jan David <jandavid@6a64.com>
2019-05-03 11:44:13 +0200

    Unlock sites for a brief period of time
    Sites are unlocked for a short period of time, after which the dialog is
    presented to the user again.

--- background.js
+++ background.js
@@ -1,13 +1,38 @@
-function redirect(requestDetails) {
-  let url = encodeURIComponent(requestDetails.url);
+const TIMEOUT = 15 * 60 * 1000; // 15 minutes
 
+function intercept(requestDetails) {
+  let encoded_url = encodeURIComponent(requestDetails.url);
+  let url = new URL(requestDetails.url);
+  let host = url.hostname;
+
+  let data_promise = browser.storage.local.get(host);
+
+  return data_promise.then((items) => {
+    let data = items[host];
+    console.log(data.unlocked_at);
+
+    if (typeof data == "undefined") {
+      return redirect(encoded_url);
+    }
+
+    let unlocked_at = new Date(data.unlocked_at);
+
+    if ((Date.now() - unlocked_at) > TIMEOUT) {
+      return redirect(encoded_url);
+    }
+  }).catch(() => {
+    return redirect(encoded_url);
+  });
+}
+
+function redirect(url) {
   return {
     redirectUrl: chrome.extension.getURL("eventually.html?url=" + url)
   };
 }
 
 browser.webRequest.onBeforeRequest.addListener(
-  redirect,
+  intercept,
   {urls: ["<all_urls>"], types: ["main_frame"]},
   ["blocking"]
 );
--- eventually.js
+++ eventually.js
@@ -2,5 +2,17 @@ let url_param = new URL(window.location.href).searchParams.get("url");
 let decoded_url = decodeURIComponent(url_param);
 let host = new URL(decoded_url).hostname;
 
+document.getElementById('eventually-button-js').addEventListener("click", unlock);
 document.getElementById('eventually-url-js').innerText = host;
-document.getElementById('eventually-link-js').setAttribute('href', decoded_url);
+
+function unlock() {
+  let data = {
+    host: host,
+    url: decoded_url,
+    unlocked_at: Date.now()
+  };
+
+  browser.storage.local.set({[host]: data}).then(() => {
+    window.location.replace(decoded_url);
+  });
+}
